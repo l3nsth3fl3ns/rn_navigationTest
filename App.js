8879df43f9fbd623eac53e9a4daf3d6d1d252372
lens <l3ns@protonmail.com>
2023-05-16 15:42:15 +0200

    Added a nested drawer navigator in App,js

--- App.js
+++ App.js
@@ -2,13 +2,40 @@ import {StatusBar} from 'expo-status-bar';
 import {Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {createDrawerNavigator} from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+// declared to pass into Stack.Screen to nest drawer navigator inside of it. As it is both navigator headers are shown.
+// to remove the stack header
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: {backgroundColor: '#303030'},
+                headerTintColor: '#CC9933',
+                drawerActiveTintColor: '#CC9933',
+                drawerContentStyle: {backgroundColor: '#F4F4F4'},
+                sceneContainerStyle: {backgroundColor: '#24180f'} // == contentStyle in Stack Navigation
+            }}>
+            <Drawer.Screen
+                name='Categoriess'
+                component={CategoriesScreen}
+                options={{
+                    title: 'All Categories'
+                }}
+            />
+            <Drawer.Screen name='Favorites' component={FavoritesScreen}/>
+        </Drawer.Navigator>
+    )
+}
 
 export default function App() {
     return (
@@ -23,12 +50,13 @@ export default function App() {
                     }}
                 >
                     <Stack.Screen
-                        name='MealsCategories'
-                        component={CategoriesScreen}
+                        name='DrawerNavigatorLoader'
+                        component={DrawerNavigator}
                         // route and navigation are passed down to screen by React Navigation automatically
                         // options function executes as soon as screen gets active
                         options={{
-                            title: 'All Categories'
+                            // title: 'All Categories'
+                            headerShown: false,
                         }}
                     />
                     <Stack.Screen
